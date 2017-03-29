7647ca5f1cdc03310a769f34494ba608644ae837
Danielvy01 <daniel.vargas.yalico@gmail.com>
2017-03-28 23:56:38 -0500

    v3

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,13 +3,13 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-//importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
-//importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
 
 // Initialize the Firebase app in the service worker by passing in the
 // messagingSenderId.
 firebase.initializeApp({
-  'messagingSenderId': 'YOUR-SENDER-ID'
+    messagingSenderId: "648531037710"
 });
 
 // Retrieve an instance of Firebase Messaging so that it can handle background
